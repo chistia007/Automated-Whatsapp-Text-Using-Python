3002f5daf94f44dabcfb9f6dd5ace5bafa9d9120
Chisthia Khan <chisti.khan1999@gmail.com>
2023-02-17 12:39:13 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ import pyautogui as gui
 import time
 
 # # Send a message to the specified contact
-# kit.sendwhatmsg("+8801731538632", "This is an automated message", 11, 47)
+kit.sendwhatmsg("+8801731538632", "This is an automated message", 11, 47)
 
 # # Wait for the message window to open and the message to be typed
-# time.sleep(5)
+time.sleep(5)
 
 # # Simulate pressing the enter key to send the message
-# gui.press("enter")
+gui.press("enter")
 
 
 
-# def on_message_received(message):
+def on_message_received(message):
 #     # Process the message and generate a reply
-#     reply = "Thanks for your message! I am a bot and I am currently unable to respond to individual messages. Please contact the administrator of this group if you have any questions or concerns."
+     reply = "Thanks for your message! I am a bot and I am currently unable to respond to individual messages. Please contact the administrator of this group if you have any questions or concerns."
 
 #     # Send the reply to the same group that the message was received from
-#     kit.sendwhatmsg_to_group("Group Name", reply, 0, 0)
+     kit.sendwhatmsg_to_group("Group Name", reply, 0, 0)
 
 # # Use the listen() function to start listening for incoming messages in the specified group
-# kit.listen(on_message_received, group_name="Group Name")
+kit.listen(on_message_received, group_name="Group Name")
 
 
 
